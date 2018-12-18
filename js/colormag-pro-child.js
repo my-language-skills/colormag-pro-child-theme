d2018f6cc9d0dbc888eb90a5c56039bcb4b3e1e2
Christodoulos Demetriou <37778768+tooulakis13@users.noreply.github.com>
2018-12-18 12:16:44 +0100

    Fixed issue that was causing the logo banner to be unclickable

--- js/colormag-pro-child.js
+++ js/colormag-pro-child.js
@@ -1,6 +1,32 @@
+var x = window.matchMedia("(max-width: 768px)");
+
 window.onload = addHomeScreenButton();
 
 function addHomeScreenButton() {
 
     jQuery('#header-text-nav-container .news-bar .inner-wrap').prepend('<a href="https://www.comunicavalencia.es" id="multipageMasterHome"></a>');
+
+    colormagproChildResize(x); // Call listener function at run time
+    x.addListener(colormagproChildResize); // Attach listener function on state changes 
+
+}
+
+window.addEventListener("resize", colormagproChildResize);
+
+function colormagproChildResize() {
+
+    if (x.matches) { // If media query matches
+        var w = window.innerWidth;
+        var wtemp = w / 10;
+        w = w - wtemp;
+        document.getElementById("site-navigation").style.width = "50px";
+        document.getElementById("site-navigation").style.padding_left = "3%";
+        document.getElementById("menu-primary").style.width = w;
+        document.getElementById("menu-primary").style.overflow = "hidden";
+        
+    } else {
+        document.getElementById("menu-primary").style.width = "100%";
+        document.getElementById("site-navigation").style.width = "100%";
+        document.getElementById("site-navigation").style.padding_left = "0";
+    }
 }
